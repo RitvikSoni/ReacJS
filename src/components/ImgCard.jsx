917a06564922667e771a9de5e18584420ea1aac0
RitvikSoni <ritviksoni1@gmail.com>
2023-08-25 17:08:30 +0530

    react

--- src/components/ImgCard.jsx
+++ src/components/ImgCard.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+
+
 const ImgCard = (props) => {
+  const [Count,setCount ]= useState(100);
+  const handleClick=()=>{
+    setCount(Count+1);
+  console.log("button is click",Count);
+  
+}
   return (
     <div>
+      
        <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={props.imgUrl}/>
       <Card.Body>
-        <Card.Title>{props.Title}</Card.Title>
+        <Card.Title>{props.title}{Count}</Card.Title>
         <Card.Text>
           {props.desc}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary" onClick={handleClick}>Go somewhere</Button>
+        {Count}
       </Card.Body>
     </Card>
     </div>
--- src/components/data.js
+++ src/components/data.js
@@ -5,9 +5,9 @@ const data = [
         desc:'Nature Images & Pictures. We handpicked more than 1,200,000 nature pictures for your choosing. HD to 4K quality images, all for free! Royalty-free images.'
         
     },
-    {
+    { 
         title:'Card 2',
-        imgUrl:'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80',
+        imgUrl:'https://images.unsplash.com/photo-1495344517868-8ebaf0a2044a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2VhcmNofGVufDB8fDB8fHww&w=1000&q=80',
         desc:'Nature Images & Pictures. We handpicked more than 1,200,000 nature pictures for your choosing. HD to 4K quality images, all for free! Royalty-free images.'
         
     },
