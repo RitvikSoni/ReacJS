1a50059e15dfcc72a96263208751194ca600c8e4
RitvikSoni <ritviksoni1@gmail.com>
2023-08-26 17:24:25 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React from "react";
 import './App.css';
 import NavBar from './components/Nav_Bar';
-import Home_Carousel from "./components/Home_Carousel";
+import HomeCarousel from "./components/HomeCarousel";
 import ImgCard from "./components/ImgCard";
-import data from "./components/data";
+ //import data from "./components/data";
+import { useState } from "react";
+import { useEffect } from "react";
+
 
 
 function App() {
+    const [data,Setdata]=useState([])
+  
+   useEffect(()=>{
+    const fetchdata=async()=>{
+      try{
+       const response = await fetch ("https://jsonplaceholder.typicode.com/photos")
+       if(!response.ok){
+        throw new  Error ("Network Response Failed")
+       }
+       const d = await response.json ()
+       console.log(data);
+       Setdata(d)
+      }
+      catch (error){
+        console.log("error accured",error)
+      }
+    }
+     fetchdata() 
+    })
+  
+
   return (
  
   <div>
     <NavBar/>
-     <Home_Carousel/>
+     <HomeCarousel/>
     <div className="image">
       {
         data.map((item)=>{
-       return <ImgCard title={item.title} imgUrl={item.imgUrl} desc={item.desc}/>
+       return <ImgCard title={item.title} imgUrl={item.url} desc={item.desc}/>
         })
       }
        
--- src/components/HomeCarousel.jsx
+++ src/components/HomeCarousel.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 
 
-function Home_Carousel() {
+function HomeCarousel() {
   return (
     <div>
        <Carousel>
       <Carousel.Item>
-        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" width={"100%"} height={"100%"} />
+        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt='' width={"100%"} height={"100%"} />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg"  width={"100%"} height={"100%"} />
+        <img src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg" alt='' width={"100%"} height={"100%"} />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src="https://www.w3schools.com/css/img_5terre_wide.jpg"  width={"100%"} height={"100%"}/>
+        <img src="https://www.w3schools.com/css/img_5terre_wide.jpg" alt='' width={"100%"} height={"100%"}/>
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
@@ -34,4 +34,4 @@ function Home_Carousel() {
   );
 }
 
-export default Home_Carousel;
+export default HomeCarousel;
